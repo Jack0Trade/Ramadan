68fcd24090e8b010dab1f2c952b71a9e0c744163
Jack0Trade <amirul.myl@gmail.com>
2025-02-02 03:33:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 function ramadanCountdown() {
-    const ramadanDate = new Date('March 10, 2024 00:00:00').getTime();
+    // Set the upcoming Ramadan start date (adjust yearly)
+    const ramadanDate = new Date('February 28, 2025 00:00:00').getTime();
     const now = new Date().getTime();
     const diff = ramadanDate - now;
 
     if (diff > 0) {
-        const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-        document.getElementById('countdown').innerText = days + " days left!";
+        const days = Math.ceil(diff / (1000 * 60 * 60 * 24));
+        document.getElementById('countdown').innerText = days + " days left until Ramadan!";
     } else {
         document.getElementById('countdown').innerText = "Ramadan is here!";
     }
 }
+
+// Update countdown daily
+setInterval(ramadanCountdown, 86400000); // 86400000 ms = 1 day
 ramadanCountdown();
